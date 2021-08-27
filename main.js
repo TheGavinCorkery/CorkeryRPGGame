2b51e4b50037d40a20a455b444c491bc6071ab2f
Gavin Corkery <GavinCorkery@hotmail.com>
2021-08-27 12:12:42 -0400

    Added function for user to select attack from prompt menu.

--- main.js
+++ main.js
@@ -2,20 +2,29 @@
 
 //Will be used to run the game in logical order
 function runGame() {
-
 }
 
 //Create the hercules object
 let hercules = {
     health: 20,
     atkPower: 5,
+    chosenAttackDmg: 0,
+    chosenAttackName: '',
     attacks: [
         {name: 'punch(light attack: 2 dmg)', damage: 2},
         {name: 'stab(normal attack: 5 dmg)', damage: 5},
         {name: 'flying dagger(heavy attack: 6 dmg)', damage: 6}
     ],
     selectAttack() {
-        alert('What attack will you use?');
+        let chosenAttack = prompt('What attack would you like to use? \n1. ' + this.attacks[0].name + '\n2. ' + 
+            this.attacks[1].name + '\n3. ' + this.attacks[2].name);
+        
+        this.chosenAttackDmg = this.attacks[chosenAttack - 1].damage;
+        this.chosenAttackName = this.attacks[chosenAttack - 1].name;
+        
+    },
+    printAttack(chosenAttack) {
+        prompt('You chose to use ' + this.attacks[chosenAttack]);
     }
 };
 // Creation of the first enemy
@@ -48,10 +57,6 @@ let storyObjects = {
         'Fantastic job! Now that you have completed the mission, you can head back to the King to find out what is in store next!',
         '"Glad to hear you have done well, next I ask you to defeat the nine-headed Lernaean Hydra, I suspect that you are the only one for the job. Do not let us down."',
         ]
-        // {order: 1, message: '"Your first mission will be to slay the ruthless Nemean Lion." - King Eurystheus'},
-        // {order: 2, message: 'Fantastic job! Now that you have completed the mission, you can head back to the King to find out what is in store next!'}, 
-        // {order: 3, message: '"Glad to hear you have done well, next I ask you to defeat the nine-headed Lernaean Hydra, I suspect that you are the only one for the job. Do not let us down."'},
-        // {order: 4, message: ''}
     ,
     
     //Prints the messages at x, where x is the point of the story you are at which is saved by a variable in the 
@@ -61,6 +66,4 @@ let storyObjects = {
     }
 };
 
-let x = 0;
-x = storyObjects.printStoryAlert(x);
-console.log(x);
+runGame();
